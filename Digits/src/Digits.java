a3b9c80125cb823dcc6d50b214c847d61e5a1207
Egor <imnotfinland@gmail.com>
2022-02-08 13:39:03 +0300

    Remove comments

--- Digits/src/Digits.java
+++ Digits/src/Digits.java
@@ -1,8 +1,6 @@
 public class Digits {
     public static int mod ( int a, int b )
     {
-        // \u000a
-        // //System.out.println("HIIIII");
         return ( a - ( a / b ) * b  );
     }
     public static void main( String [] args )
