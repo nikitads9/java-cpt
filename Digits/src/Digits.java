08ecb7b0bf4000e5b33c5dae5828b542128ca9c5
Egor <imnotfinland@gmail.com>
2022-02-08 14:03:08 +0300

    Some questions

--- Digits/src/Digits.java
+++ Digits/src/Digits.java
@@ -6,5 +6,6 @@ public class Digits {
     public static void main( String [] args )
     {
         System.out.println ( "Hello World!!!" );
+        System.out.println (" Git the best?");
     }
 }
