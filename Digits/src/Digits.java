f351491b948af53139ca391a58f0b337e037b844
chopas11 <alexander.nikiforov.02@gmail.com>
2022-02-08 19:40:46 +0300

    Change Indentation style

--- Digits/src/Digits.java
+++ Digits/src/Digits.java
@@ -1,10 +1,8 @@
 public class Digits {
-    public static int mod ( int a, int b )
-    {
+    public static int mod ( int a, int b ) {
         return ( a - ( a / b ) * b  );
     }
-    public static void main( String [] args )
-    {
+    public static void main( String [] args ) {
         System.out.println ( "Hello World!!!" );
     }
 }
