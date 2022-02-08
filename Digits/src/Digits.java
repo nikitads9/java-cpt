25731241c9f51ce5acc3b8df01bb2bd81f4a6772
Egor <imnotfinland@gmail.com>
2022-02-08 22:42:48 +0300

    SecondLastResort

--- Digits/src/Digits.java
+++ Digits/src/Digits.java
@@ -1,8 +1,13 @@
 public class Digits {
     public static int mod ( int a, int b ) {
         return ( a - ( a / b ) * b  );
+        System.out.println ( "Hello World!!!" );
+        System.out.println ( "Hello World!!!" );
+        System.out.println ( "Hello World!!!" );
+        System.out.println ( "Hello World!!!" );
     }
     public static void main( String [] args ) {
+
         System.out.println ( "Hello World!!!" );
         System.out.println ( "Hello World!!!" );
         System.out.println ( "Hello World!!!" );
