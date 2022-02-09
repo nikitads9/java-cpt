47ac00c69e03c958281f980444bcdcdb3c378441
Egor <imnotfinland@gmail.com>
2022-02-09 23:28:14 +0300

    We need more Tasks...

--- Digits/src/Digits.java
+++ Digits/src/Digits.java
@@ -6,6 +6,7 @@ public class Digits {
     public static void main( String [] args )
     {
         System.out.println ( "Task 10" );
-
+        System.out.println ( "Task 10" );
+        System.out.println ( "Task 10" );
     }
 }
