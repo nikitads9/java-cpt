f50f8f34032c71584d9c29f92460e92d9ee8d51f
Nikita Denisenok <nikitadenisenok@gmail.com>
2022-02-17 23:36:09 +0300

    add new name

--- Lab_1/src/Palindrome.java
+++ Lab_1/src/Palindrome.java
@@ -1,9 +1,7 @@
 public class Palindrome { //Программа считывает полученные через терминал слова и проверяет является ли каждое слово палиндромом
     public static void main(String[] args)  {
-        //String[] initial = {"meme", "lol", "tenet"};
 
         for (int i = 0; i < args.length; i++) { //цикл по элеметам массива строк, переданному при вызове функции в терминале
-
                 String s = args[i]; //запписываем в s новое слово
                 String reversed = reverseString(s);
                 System.out.println(s);
@@ -14,6 +12,7 @@ public class Palindrome { //Программа считывает получен
         } 
     }
     public static String reverseString(String s) { //функция возвращает инвертированную строку
+        
         String reversed = ""; //Пустая строка в начале
         for (int i = s.length() - 1; i >= 0; i--) { //обратный цикл с постдекрементом
             reversed += s.charAt(i); //в строку реверсд записываются char'ы. Так как строка - массив чаров, то можно при помощи конкатенации собрать строку.
@@ -21,7 +20,8 @@ public class Palindrome { //Программа считывает получен
         return reversed;
     }
     public static boolean isPalindrome(String s, String reversed) { //метод возвращает правда или нет
-        if (s.equals(reversed)) { //метод проверки равенства применяется к объекту s, аршументом передается сравниваемая строка
+        
+        if (s.equals(reversed)) { //метод проверки равенства применяется к объекту s, аргументом передается сравниваемая строка
             return true;
         }
         return false;  
